a73e4ec619844b109bef91ec3c09ea26b25b50ba
Sahand Kashani-Akhavan <sahand.kashani@gmail.com>
2015-03-24 09:16:40 +0100

    Modified pointer parameters in functions to their array equivalent to know how many elements are in each element.

--- utilities.c
+++ utilities.c
@@ -4,7 +4,7 @@
 #include "constants.h"
 #include "prime_field_arithmetic.h"
 
-void print_num(uint64_t const * const num) {
+void print_num(uint64_t const num[NUM_LIMBS]) {
 	for (int i = NUM_LIMBS-1; i >= 0; i--) {
 		printf("%0*" PRIx64, LIMB_SIZE_IN_HEX, num[i]);
 	}
@@ -15,24 +15,24 @@ void print_num_gmp(mpz_t const num_gmp) {
 	gmp_printf("%0*Zx\n", PRIME_FIELD_FULL_HEX_LENGTH, num_gmp);
 }
 
-void clear_num(uint64_t * const num) {
+void clear_num(uint64_t num[NUM_LIMBS]) {
 	for (unsigned int i = 0; i < NUM_LIMBS; i++) {
 		num[i] = 0;
 	}
 }
 
-void convert_gmp_to_num(uint64_t * const num, mpz_t const num_gmp) {
+void convert_gmp_to_num(uint64_t num[NUM_LIMBS], mpz_t const num_gmp) {
 	// must clear the number, because GMP will only fill enough words that is
 	// needed, so the last words of num may not be set automatically.
 	clear_num(num);
 	mpz_export(num, NULL, -1, LIMB_SIZE_IN_BYTES, 0, 0, num_gmp);
 }
 
-void convert_num_to_gmp(mpz_t num_gmp, uint64_t const * const num) {
+void convert_num_to_gmp(mpz_t num_gmp, uint64_t const num[NUM_LIMBS]) {
 	mpz_import(num_gmp, NUM_LIMBS, -1, LIMB_SIZE_IN_BYTES, 0, 0, num);
 }
 
-bool is_equal_num_num(uint64_t const * const num1, uint64_t const * const num2) {
+bool is_equal_num_num(uint64_t const num1[NUM_LIMBS], uint64_t const num2[NUM_LIMBS]) {
 	for (unsigned int i = 0; i < NUM_LIMBS; i++) {
 		if (num1[i] != num2[i]) {
 			return false;
@@ -41,19 +41,19 @@ bool is_equal_num_num(uint64_t const * const num1, uint64_t const * const num2) 
 	return true;
 }
 
-bool is_equal_num_gmp(uint64_t const * const num, mpz_t const num_gmp) {
+bool is_equal_num_gmp(uint64_t const num[NUM_LIMBS], mpz_t const num_gmp) {
 	uint64_t tmp[NUM_LIMBS];
 	convert_gmp_to_num(tmp, num_gmp);
 	return is_equal_num_num(tmp, num);
 }
 
-int cmp_num_gmp(uint64_t const * const num, mpz_t const num_gmp) {
+int cmp_num_gmp(uint64_t const num[NUM_LIMBS], mpz_t const num_gmp) {
 	uint64_t tmp[NUM_LIMBS];
 	convert_gmp_to_num(tmp, num_gmp);
 	return cmp(num, tmp);
 }
 
-void generate_random_gmp(mpz_t num_gmp, unsigned int const precision_in_bits, uint64_t const * const strict_upper_bound, gmp_randstate_t gmp_random_state) {
+void generate_random_gmp(mpz_t num_gmp, unsigned int const precision_in_bits, uint64_t const strict_upper_bound[NUM_LIMBS], gmp_randstate_t gmp_random_state) {
 	do {
 		mpz_urandomb(num_gmp, gmp_random_state, precision_in_bits);
 	} while (cmp_num_gmp(strict_upper_bound, num_gmp) != 1);
--- utilities.h
+++ utilities.h
@@ -4,15 +4,16 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <gmp.h>
+#include "constants.h"
 
-void print_num(uint64_t const * const num);
+void print_num(uint64_t const num[NUM_LIMBS]);
 void print_num_gmp(mpz_t const num_gmp);
-void clear_num(uint64_t * const num);
-void convert_gmp_to_num(uint64_t * const num, mpz_t const num_gmp);
-void convert_num_to_gmp(mpz_t num_gmp, uint64_t const * const num);
-bool is_equal_num_num(uint64_t const * const num1, uint64_t const * const num2);
-bool is_equal_num_gmp(uint64_t const * const num, mpz_t const num_gmp);
-int cmp_num_gmp(uint64_t const * const num, mpz_t const num_gmp);
-void generate_random_gmp(mpz_t num_gmp, unsigned int const precision_in_bits, uint64_t const * const strict_upper_bound, gmp_randstate_t gmp_random_state);
+void clear_num(uint64_t num[NUM_LIMBS]);
+void convert_gmp_to_num(uint64_t num[NUM_LIMBS], mpz_t const num_gmp);
+void convert_num_to_gmp(mpz_t num_gmp, uint64_t const num[NUM_LIMBS]);
+bool is_equal_num_num(uint64_t const num1[NUM_LIMBS], uint64_t const num2[NUM_LIMBS]);
+bool is_equal_num_gmp(uint64_t const num[NUM_LIMBS], mpz_t const num_gmp);
+int cmp_num_gmp(uint64_t const num[NUM_LIMBS], mpz_t const num_gmp);
+void generate_random_gmp(mpz_t num_gmp, unsigned int const precision_in_bits, uint64_t const strict_upper_bound[NUM_LIMBS], gmp_randstate_t gmp_random_state);
 
 #endif
