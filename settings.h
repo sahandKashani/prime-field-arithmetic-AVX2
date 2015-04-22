2ec4ce9636f93b4dd4490d67c2d4ff404a846b4b
Sahand Kashani-Akhavan <sahand.kashani@gmail.com>
2015-04-22 11:53:56 +0200

    MULX and AVX will no longer be needed. AVX is replaced by SIMD_PARALLEL_WALKS. MULX is automatically generated by compiler when using unsigned __int128

--- settings.h
+++ settings.h
@@ -3,14 +3,13 @@
 
 #define PRIME_FIELD_BINARY_BIT_LENGTH        (131)
 #define LIMB_SIZE_IN_BITS                    (64)
-#define FULL_LIMB_PRECISION                  (0)
+#define FULL_LIMB_PRECISION                  (1)
 
 #define BRANCHLESS_MODULAR_ADDITION          (1)
 #define BRANCHLESS_MODULAR_SUBTRACTION       (1)
 #define BRANCHLESS_MONTGOMERY_MULTIPLICATION (1)
-#define MULX                                 (0)
-#define AVX                                  (0)
-#define AVX_ALIGNED_MEMORY                   (0)
+
 #define SIMD_PARALLEL_WALKS                  (0)
+#define AVX_ALIGNED_MEMORY                   (0)
 
 #endif
