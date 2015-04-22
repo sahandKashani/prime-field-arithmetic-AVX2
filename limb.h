77a4acd228732de43c17620e807dfb0dd7f9b7a8
Sahand Kashani-Akhavan <sahand.kashani@gmail.com>
2015-04-22 09:58:39 +0200

    New limb.h header file

---
+++ limb.h
@@ -0,0 +1,15 @@
+#ifndef LIMB_H_
+#define LIMB_H_
+
+#include "settings.h"
+
+#if LIMB_SIZE_IN_BITS == 32
+typedef uint32_t limb_t;
+typedef uint64_t dlimb_t;
+#elif LIMB_SIZE_IN_BITS == 64
+typedef unsigned __int128 uint128_t;
+typedef uint64_t limb_t;
+typedef uint128_t dlimb_t;
+#endif
+
+#endif
