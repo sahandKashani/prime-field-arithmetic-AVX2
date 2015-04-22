e036af012586062c2416a775ea0ed152f8016b31
Sahand Kashani-Akhavan <sahand.kashani@gmail.com>
2015-04-22 10:04:57 +0200

    Added macro for generic limb printing.

--- limb.h
+++ limb.h
@@ -1,15 +1,18 @@
 #ifndef LIMB_H_
 #define LIMB_H_
 
+#include <inttypes.h>
 #include "settings.h"
 
 #if LIMB_SIZE_IN_BITS == 32
 typedef uint32_t limb_t;
 typedef uint64_t dlimb_t;
+#define PRI_LIMB (PRIx32)
 #elif LIMB_SIZE_IN_BITS == 64
 typedef unsigned __int128 uint128_t;
 typedef uint64_t limb_t;
 typedef uint128_t dlimb_t;
+#define PRI_LIMB (PRIx64)
 #endif
 
 #endif
