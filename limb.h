1518ef2eaec6262be1feaa7757c3625339da327e
Sahand Kashani-Akhavan <sahand.kashani@gmail.com>
2015-04-22 10:06:23 +0200

    No parenthesis around string concatenations destined towards preprocessor

--- limb.h
+++ limb.h
@@ -7,12 +7,12 @@
 #if LIMB_SIZE_IN_BITS == 32
 typedef uint32_t limb_t;
 typedef uint64_t dlimb_t;
-#define PRI_LIMB (PRIx32)
+#define PRI_LIMB PRIx32
 #elif LIMB_SIZE_IN_BITS == 64
 typedef unsigned __int128 uint128_t;
 typedef uint64_t limb_t;
 typedef uint128_t dlimb_t;
-#define PRI_LIMB (PRIx64)
+#define PRI_LIMB PRIx64
 #endif
 
 #endif
