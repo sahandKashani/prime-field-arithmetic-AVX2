abeab005f4f65ed5e908ee75d3983d1e943642fe
Sahand Kashani-Akhavan <sahand.kashani@gmail.com>
2015-04-23 14:51:20 +0200

    Added vectorized addition code.

--- arithmetic_vector.c
+++ arithmetic_vector.c
@@ -1,61 +1,130 @@
 #include <immintrin.h>
 #include "constants.h"
-#include "settings.h"
-#include "limb.h"
 #include "arithmetic_vector.h"
+#include "limb.h"
+#include "settings.h"
+#include "utilities.h"
 
-//unsigned int add(limb_t * const c, limb_t const * const a, limb_t const * const b, unsigned int const num_limbs, unsigned int const carry_in) {
-//    unsigned int carry_out = carry_in;
-//
-//    for (unsigned int i = 0; i < num_limbs; i++) {
-//        // we need to temporarily store the output of each operation, because it
-//        // is possible that c is the same array as a or b.
-//        limb_t c_tmp = 0;
-//
-//        #if FULL_LIMB_PRECISION
-//
-//        c_tmp = a[i] + carry_out;
-//        carry_out = (c_tmp < a[i]);
-//        c_tmp += b[i];
-//        carry_out |= (c_tmp < b[i]);
-//
-//        #else
-//
-//        c_tmp = a[i] + b[i] + carry_out;
-//        carry_out = carry(c_tmp);
-//        c_tmp = reduce_to_base(c_tmp);
-//
-//        #endif
-//
-//        c[i] = c_tmp;
-//    }
-//
-//    return carry_out;
-//}
+#if !FULL_LIMB_PRECISION
 
-unsigned int add_simd(limb_vec_t * const c, limb_vec_t const * const a, limb_vec_t const * const b, unsigned int const num_limbs, limb_vec_t const carry_in) {
-    __m256i carry_out = carry_in;
+limb_vec_t carry_vector(limb_vec_t limb) {
+    // return (unsigned int) (limb >> BASE_EXPONENT) & 0x1;
+    limb_vec_t mask = _mm256_set1_epi32(0x1);
+    limb_vec_t tmp = _mm256_srli_epi32(limb, BASE_EXPONENT);
+    return _mm256_and_si256(tmp, mask);
+}
+
+limb_vec_t reduce_to_base_vector(limb_vec_t limb) {
+    // return limb & (((limb_t) -1) >> (LIMB_SIZE_IN_BITS - BASE_EXPONENT));
+    limb_vec_t mask = _mm256_set1_epi32(-1);
+    mask = _mm256_srli_epi32(mask, LIMB_SIZE_IN_BITS - BASE_EXPONENT);
+    return _mm256_and_si256(limb, mask);
+}
+
+#endif
+
+limb_vec_t set_vector(limb_t a) {
+    #if LIMB_SIZE_IN_BITS == 32
+
+    return _mm256_set1_epi32((int) a);
+
+    #elif LIMB_SIZE_IN_BITS == 64
+
+    return _mm256_set1_epi64x((long long int) a);
+
+    #endif
+}
+
+limb_vec_t add_vector(limb_vec_t a, limb_vec_t b) {
+    #if LIMB_SIZE_IN_BITS == 32
+
+    return _mm256_add_epi32(a, b);
+
+    #elif LIMB_SIZE_IN_BITS == 64
+
+    return _mm256_add_epi64(a, b);
+
+    #endif
+}
+
+limb_vec_t cmpgt_vector(limb_vec_t a, limb_vec_t b) {
+    limb_vec_t tmp;
+    limb_vec_t mask = set_vector(0x1);
+
+    #if LIMB_SIZE_IN_BITS == 32
+
+    tmp = _mm256_cmpgt_epi32(a, b);
+
+    #elif LIMB_SIZE_IN_BITS == 64
+
+    tmp = _mm256_cmpgt_epi64(a, b);
+
+    #endif
+
+    return and_vector(tmp, mask);
+}
+
+limb_vec_t load_vector(limb_vec_t const *addr) {
+    return _mm256_load_si256(addr);
+}
+
+limb_vec_t zero_vector() {
+    return _mm256_setzero_si256();
+}
+
+limb_vec_t or_vector(limb_vec_t a, limb_vec_t b) {
+    return _mm256_or_si256(a, b);
+}
+
+limb_vec_t and_vector(limb_vec_t a, limb_vec_t b) {
+    return _mm256_and_si256(a, b);
+}
+
+void store_vector(limb_vec_t * const addr, limb_vec_t data) {
+    _mm256_storeu_si256(addr, data);
+}
+
+limb_vec_t add_simd(limb_vec_t * const c, limb_vec_t const * const a, limb_vec_t const * const b, unsigned int const num_limbs, limb_vec_t const carry_in) {
+    limb_vec_t carry_out = carry_in;
 
     for (unsigned int i = 0; i < num_limbs; i++) {
         // we need to temporarily store the output of each operation, because it
         // is possible that c is the same array as a or b.
-        limb_vec_t c_tmp = _mm256_setzero_si256();
+        // limb_t c_tmp = 0;
+        limb_vec_t c_tmp = zero_vector();
 
-        limb_vec_t a_i = _mm256_load_si256(a + i);
-        limb_vec_t b_i = _mm256_load_si256(b + i);
+        limb_vec_t a_i = load_vector(a + i);
+        limb_vec_t b_i = load_vector(b + i);
 
         #if FULL_LIMB_PRECISION
 
-        c_tmp = _mm256_add_epi32(a_i, carry_out);
-        carry_out = _mm256_cmpgt_epi32(a_i, c_tmp);
-        c_tmp = _mm256_add_epi32(c_tmp, b_i);
-//        carry_out = _mm256_or_si256();
+        // c_tmp = a[i] + carry_out;
+        c_tmp = add_vector(a_i, carry_out);
+
+        // carry_out = (c_tmp < a[i]);
+        carry_out = cmpgt_vector(a_i, c_tmp);
+
+        // c_tmp += b[i];
+        c_tmp = add_vector(c_tmp, b_i);
+
+        // carry_out |= (c_tmp < b[i]);
+        carry_out = or_vector(carry_out, cmpgt_vector(b_i, c_tmp));
 
         #else
 
+        // c_tmp = a[i] + b[i] + carry_out;
+        c_tmp = add_vector(a_i, add_vector(b_i, carry_out));
+
+        // carry_out = carry(c_tmp);
+        carry_out = carry_vector(c_tmp);
+
+        // c_tmp = reduce_to_base(c_tmp);
+        c_tmp = reduce_to_base_vector(c_tmp);
 
         #endif
 
+        store_vector(c + i, c_tmp);
     }
 
+    return carry_out;
 }
--- arithmetic_vector.h
+++ arithmetic_vector.h
@@ -1,6 +1,24 @@
 #ifndef ARITHMETIC_VECTOR_H_
 #define ARITHMETIC_VECTOR_H_
 
+#include "limb.h"
 
+#if !FULL_LIMB_PRECISION
+
+limb_vec_t carry_vector(limb_vec_t limb);
+limb_vec_t reduce_to_base_vector(limb_vec_t limb);
+
+#endif
+
+limb_vec_t set_vector(limb_t a);
+limb_vec_t add_vector(limb_vec_t a, limb_vec_t b);
+limb_vec_t cmpgt_vector(limb_vec_t a, limb_vec_t b);
+limb_vec_t load_vector(limb_vec_t const *addr);
+limb_vec_t zero_vector();
+limb_vec_t or_vector(limb_vec_t a, limb_vec_t b);
+limb_vec_t and_vector(limb_vec_t a, limb_vec_t b);
+void store_vector(limb_vec_t * const addr, limb_vec_t data);
+
+limb_vec_t add_simd(limb_vec_t * const c, limb_vec_t const * const a, limb_vec_t const * const b, unsigned int const num_limbs, limb_vec_t const carry_in);
 
 #endif
