abb1c586e495a1188f308641978925797f250bce
Sahand Kashani-Akhavan <sahand.kashani@gmail.com>
2015-04-29 17:59:48 +0200

    header file for previous arithmetic.c commit.

--- arithmetic.h
+++ arithmetic.h
@@ -6,23 +6,22 @@
 
 #if !FULL_LIMB_PRECISION
 
-limb_t carry(limb_t limb);
-limb_t reduce_to_base(limb_t limb);
-limb_t excess_base_bits(limb_t limb);
+    limb_t carry(limb_t limb);
+    limb_t reduce_to_base(limb_t limb);
+    limb_t excess_base_bits(limb_t limb);
 
 #endif
 
-limb_t add(limb_t * const c, limb_t const * const a, limb_t const * const b, unsigned int const num_limbs, limb_t const carry_in);
-limb_t add_num_limb(limb_t * const c, limb_t const * const a, limb_t const b, unsigned int const num_limbs, limb_t const carry_in);
-limb_t sub(limb_t * const c, limb_t const * const a, limb_t const * const b, unsigned int const num_limbs, limb_t const borrow_in);
-void mul_limb_limb(limb_t * const c_hi, limb_t * const c_lo, limb_t const a, limb_t const b);
+limb_t add_num_num(limb_t *c, limb_t *a, limb_t *b, unsigned int num_limbs, limb_t carry_in);
+limb_t add_num_limb(limb_t *c, limb_t *a, limb_t b, unsigned int num_limbs, limb_t carry_in);
+limb_t sub_num_num(limb_t *c, limb_t *a, limb_t *b, unsigned int num_limbs, limb_t borrow_in);
 void mul_num_limb(limb_t * const c, limb_t const * const a, limb_t const b, unsigned int const num_limbs);
-void mul(limb_t * const c, limb_t const * const a, limb_t const * const b, unsigned int const num_limbs);
+void mul_num_num(limb_t * const c, limb_t const * const a, limb_t const * const b, unsigned int const num_limbs);
 bool equals_zero(limb_t const * const num, unsigned int const num_limbs);
-int cmp(limb_t const * const a, limb_t const * const b, unsigned int const num_limbs);
-void and(limb_t * const c, limb_t const * const a, limb_t const * const b, unsigned int const num_limbs);
-void add_mod(limb_t * const c, limb_t const * const a, limb_t const * const b, limb_t const * const m, unsigned int const num_limbs);
-void sub_mod(limb_t * const c, limb_t const * const a, limb_t const * const b, limb_t const * const m, unsigned int const num_limbs);
-void mul_montgomery(limb_t * const z, limb_t const * const x, limb_t const * const y, limb_t const * const m, limb_t m_prime, unsigned int const num_limbs);
+int cmp_num_num(limb_t const * const a, limb_t const * const b, unsigned int const num_limbs);
+void and_num_num(limb_t * const c, limb_t const * const a, limb_t const * const b, unsigned int const num_limbs);
+void add_mod_num_num(limb_t * const c, limb_t const * const a, limb_t const * const b, limb_t const * const m, unsigned int const num_limbs);
+void sub_mod_num_num(limb_t * const c, limb_t const * const a, limb_t const * const b, limb_t const * const m, unsigned int const num_limbs);
+void mul_montgomery_num_num(limb_t * const z, limb_t const * const x, limb_t const * const y, limb_t const * const m, limb_t m_prime, unsigned int const num_limbs);
 
 #endif
