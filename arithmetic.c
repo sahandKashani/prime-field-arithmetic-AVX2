b5f19cef95846765db0eb23bac59591cbe5d1c68
Sahand Kashani-Akhavan <sahand.kashani@gmail.com>
2015-05-02 15:02:39 +0200

    more things in arithmetic.c moved to _num_num() functions

--- arithmetic.c
+++ arithmetic.c
@@ -142,159 +142,159 @@ limb_t sub_num_num(limb_t *c, limb_t *a, limb_t *b, unsigned int num_limbs, limb
 }
 
 void mul_num_limb(limb_t * const c, limb_t const * const a, limb_t const b, unsigned int const num_limbs) {
-//    limb_t res[num_limbs + 1];
-//    clear_num(res, num_limbs + 1);
-//
-//    limb_t carry_out = 0;
-//    for (unsigned int i = 0; i < num_limbs; i++) {
-//        limb_t inner_product[2] = {0, 0};
-//        mul_limb_limb(&inner_product[1], &inner_product[0], a[i], b);
-//        carry_out = add(res + i, res + i, inner_product, 2, carry_out);
-//    }
-//
-//    copy_num(c, res, num_limbs + 1);
+    limb_t res[num_limbs + 1];
+    clear_num(res, num_limbs + 1);
+
+    limb_t carry_out = 0;
+    for (unsigned int i = 0; i < num_limbs; i++) {
+        limb_t inner_product[2] = {0, 0};
+        mul_limb_limb(&inner_product[1], &inner_product[0], a[i], b);
+        carry_out = add_num_num(res + i, res + i, inner_product, 2, carry_out);
+    }
+
+    copy_num(c, res, num_limbs + 1);
 }
 
 void mul_num_num(limb_t * const c, limb_t const * const a, limb_t const * const b, unsigned int const num_limbs) {
-//    limb_t res[2 * num_limbs];
-//    clear_num(res, 2 * num_limbs);
-//
-//    for (unsigned int i = 0; i < num_limbs; i++) {
-//        limb_t tmp[num_limbs + 1];
-//        mul_num_limb(tmp, a, b[i], num_limbs);
-//        add(res + i, res + i, tmp, num_limbs + 1, 0);
-//    }
-//
-//    copy_num(c, res, 2 * num_limbs);
+    limb_t res[2 * num_limbs];
+    clear_num(res, 2 * num_limbs);
+
+    for (unsigned int i = 0; i < num_limbs; i++) {
+        limb_t tmp[num_limbs + 1];
+        mul_num_limb(tmp, a, b[i], num_limbs);
+        add_num_num(res + i, res + i, tmp, num_limbs + 1, 0);
+    }
+
+    copy_num(c, res, 2 * num_limbs);
 }
 
 bool equals_zero(limb_t const * const num, unsigned int const num_limbs) {
-//    for (unsigned int i = 0; i < num_limbs; i++) {
-//        if (num[i] != 0) {
-//            return false;
-//        }
-//    }
-//    return true;
+    for (unsigned int i = 0; i < num_limbs; i++) {
+        if (num[i] != 0) {
+            return false;
+        }
+    }
+    return true;
 }
 
 // returns -1 if a < b
 // returns  0 if a == b
 // returns +1 if a > b
 int cmp_num_num(limb_t const * const a, limb_t const * const b, unsigned int const num_limbs) {
-//    limb_t tmp[num_limbs];
-//    limb_t borrow_out = sub(tmp, a, b, num_limbs, 0);
-//
-//    if (borrow_out) {
-//        return -1;
-//    } else if (equals_zero(tmp, num_limbs)) {
-//        return 0;
-//    } else {
-//        return 1;
-//    }
+    limb_t tmp[num_limbs];
+    limb_t borrow_out = sub_num_num(tmp, a, b, num_limbs, 0);
+
+    if (borrow_out) {
+        return -1;
+    } else if (equals_zero(tmp, num_limbs)) {
+        return 0;
+    } else {
+        return 1;
+    }
 }
 
 void and_num_num(limb_t * const c, limb_t const * const a, limb_t const * const b, unsigned int const num_limbs) {
-//    for (unsigned int i = 0; i < num_limbs; i++) {
-//        c[i] = a[i] & b[i];
-//    }
+    for (unsigned int i = 0; i < num_limbs; i++) {
+        c[i] = a[i] & b[i];
+    }
 }
 
 void add_mod_num_num(limb_t * const c, limb_t const * const a, limb_t const * const b, limb_t const * const m, unsigned int const num_limbs) {
-//    #if BRANCHLESS_MODULAR_ADDITION
-//
-//    limb_t mask[num_limbs];
-//    clear_num(mask, num_limbs);
-//
-//    add(c, a, b, num_limbs, 0);
-//    limb_t borrow_out = sub(c, c, m, num_limbs, 0);
-//    sub(mask, mask, mask, num_limbs, borrow_out);
-//    and(mask, m, mask, num_limbs);
-//    add(c, c, mask, num_limbs, 0);
-//
-//    #else
-//
-//    add(c, a, b, num_limbs, 0);
-//    if (cmp(c, m, num_limbs) >= 0) {
-//        sub(c, c, m, num_limbs, 0);
-//    }
-//
-//    #endif
+    #if BRANCHLESS_MODULAR_ADDITION
+
+        limb_t mask[num_limbs];
+        clear_num(mask, num_limbs);
+
+        add_num_num(c, a, b, num_limbs, 0);
+        limb_t borrow_out = sub_num_num(c, c, m, num_limbs, 0);
+        sub_num_num(mask, mask, mask, num_limbs, borrow_out);
+        and_num_num(mask, m, mask, num_limbs);
+        add_num_num(c, c, mask, num_limbs, 0);
+
+    #else
+
+        add_num_num(c, a, b, num_limbs, 0);
+        if (cmp_num_num(c, m, num_limbs) >= 0) {
+            sub_num_num(c, c, m, num_limbs, 0);
+        }
+
+    #endif
 }
 
 void sub_mod_num_num(limb_t * const c, limb_t const * const a, limb_t const * const b, limb_t const * const m, unsigned int const num_limbs) {
-//    #if BRANCHLESS_MODULAR_SUBTRACTION
-//
-//    limb_t mask[num_limbs];
-//    clear_num(mask, num_limbs);
-//
-//    limb_t borrow_out = sub(c, a, b, num_limbs, 0);
-//    sub(mask, mask, mask, num_limbs, borrow_out);
-//    and(mask, m, mask, num_limbs);
-//    add(c, c, mask, num_limbs, 0);
-//
-//    #else
-//
-//    unsigned int borrow_out = sub(c, a, b, num_limbs, 0);
-//    if (borrow_out) {
-//        add(c, c, m, num_limbs, 0);
-//    }
-//
-//    #endif
+    #if BRANCHLESS_MODULAR_SUBTRACTION
+
+        limb_t mask[num_limbs];
+        clear_num(mask, num_limbs);
+
+        limb_t borrow_out = sub_num_num(c, a, b, num_limbs, 0);
+        sub_num_num(mask, mask, mask, num_limbs, borrow_out);
+        and_num_num(mask, m, mask, num_limbs);
+        add_num_num(c, c, mask, num_limbs, 0);
+
+    #else
+
+        unsigned int borrow_out = sub_num_num(c, a, b, num_limbs, 0);
+        if (borrow_out) {
+            add_num_num(c, c, m, num_limbs, 0);
+        }
+
+    #endif
 }
 
 void mul_montgomery_num_num(limb_t * const z, limb_t const * const x, limb_t const * const y, limb_t const * const m, limb_t m_prime, unsigned int const num_limbs) {
-//    limb_t A[num_limbs + 1];
-//    clear_num(A, num_limbs + 1);
-//
-//    for (unsigned int i = 0; i < num_limbs; i++) {
-//        // u_i = (a_0 + (x_i * y_0)) * m' mod b
-//        limb_t A0_Xi_Y0_Mprime = (A[0] + x[i] * y[0]) * m_prime;
-//
-//        #if FULL_LIMB_PRECISION
-//
-//        limb_t ui = A0_Xi_Y0_Mprime;
-//
-//        #else
-//
-//        limb_t ui = reduce_to_base(A0_Xi_Y0_Mprime);
-//
-//        #endif
-//
-//        // A = (A + (x_i * y) + (u_i * m)) / b
-//
-//        // x_i * y
-//        limb_t xi_y[num_limbs + 1];
-//        mul_num_limb(xi_y, y, x[i], num_limbs);
-//
-//        // u_i * m
-//        limb_t ui_m[num_limbs + 1];
-//        mul_num_limb(ui_m, m, ui, num_limbs);
-//
-//        // A = A + (x_i * y)
-//        add(A, A, xi_y, num_limbs + 1, 0);
-//
-//        // A = A + (x_i * y) + (u_i * m)
-//        add(A, A, ui_m, num_limbs + 1, 0);
-//
-//        // A = (A + (x_i * y) + (u_i * m)) / b
-//        for (unsigned int j = 0; j < num_limbs; j++) {
-//            A[j] = A[j + 1];
-//        }
-//        A[num_limbs] = 0;
-//    }
-//
-//    #if BRANCHLESS_MONTGOMERY_MULTIPLICATION
-//
-//    sub_mod(A, A, m, m, num_limbs);
-//
-//    #else
-//
-//    // A[num_limbs] = 0, so it is like if A and m have the same number of limbs.
-//    if (cmp(A, m, num_limbs) >= 0) {
-//        sub(A, A, m, num_limbs, 0);
-//    }
-//
-//    #endif
-//
-//    copy_num(z, A, num_limbs);
+    limb_t A[num_limbs + 1];
+    clear_num(A, num_limbs + 1);
+
+    for (unsigned int i = 0; i < num_limbs; i++) {
+        // u_i = (a_0 + (x_i * y_0)) * m' mod b
+        limb_t A0_Xi_Y0_Mprime = (A[0] + x[i] * y[0]) * m_prime;
+
+        #if FULL_LIMB_PRECISION
+
+            limb_t ui = A0_Xi_Y0_Mprime;
+
+        #else
+
+            limb_t ui = reduce_to_base(A0_Xi_Y0_Mprime);
+
+        #endif
+
+        // A = (A + (x_i * y) + (u_i * m)) / b
+
+        // x_i * y
+        limb_t xi_y[num_limbs + 1];
+        mul_num_limb(xi_y, y, x[i], num_limbs);
+
+        // u_i * m
+        limb_t ui_m[num_limbs + 1];
+        mul_num_limb(ui_m, m, ui, num_limbs);
+
+        // A = A + (x_i * y)
+        add_num_num(A, A, xi_y, num_limbs + 1, 0);
+
+        // A = A + (x_i * y) + (u_i * m)
+        add_num_num(A, A, ui_m, num_limbs + 1, 0);
+
+        // A = (A + (x_i * y) + (u_i * m)) / b
+        for (unsigned int j = 0; j < num_limbs; j++) {
+            A[j] = A[j + 1];
+        }
+        A[num_limbs] = 0;
+    }
+
+    #if BRANCHLESS_MONTGOMERY_MULTIPLICATION
+
+        sub_mod_num_num(A, A, m, m, num_limbs);
+
+    #else
+
+        // A[num_limbs] = 0, so it is like if A and m have the same number of limbs.
+        if (cmp_num_num(A, m, num_limbs) >= 0) {
+            sub_num_num(A, A, m, num_limbs, 0);
+        }
+
+    #endif
+
+    copy_num(z, A, num_limbs);
 }
