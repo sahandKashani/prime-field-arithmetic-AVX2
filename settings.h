fb135d1386a1d0ccf3dc4400ecfae8507eace8d6
Sahand Kashani-Akhavan <sahand.kashani@gmail.com>
2015-05-03 11:39:21 +0200

    remove BRANCHLESS_ settings from settings.h since we no longer can use them

--- settings.h
+++ settings.h
@@ -5,10 +5,6 @@
     #define LIMB_SIZE_IN_BITS                    (32)
     #define FULL_LIMB_PRECISION                  (0)
 
-    #define BRANCHLESS_MODULAR_ADDITION          (1)
-    #define BRANCHLESS_MODULAR_SUBTRACTION       (1)
-    #define BRANCHLESS_MONTGOMERY_MULTIPLICATION (1)
-
     #define MULX                                 (0)
 
     #define SIMD_PARALLEL_WALKS                  (0)
