9f0645221f60e98fabf38e3b7af2add4d0e436fa
Sahand Kashani-Akhavan <sahand.kashani@gmail.com>
2015-05-06 12:33:45 +0200

    definition of "vector" mpz_t type in gmp_int.h

--- gmp_int.h
+++ gmp_int.h
@@ -1,4 +1,9 @@
-#ifndef GMPI_H_
-#define GMPI_H_
+#ifndef GMP_INT_H_
+#define GMP_INT_H_
 
-#endif /* GMPI_H_ */
+    #include "constants.h"
+    #include <gmp.h>
+
+    typedef mpz_t gmp_int_t[NUM_ENTRIES_IN_LIMB];
+
+#endif /* GMPI_INT_H_ */
