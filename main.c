105448ac87dce966d8c3c263684d76cea9e282ae
Sahand Kashani-Akhavan <sahand.kashani@gmail.com>
2015-05-23 11:58:13 +0200

    sub_num_num() works

--- main.c
+++ main.c
@@ -140,68 +140,68 @@ bool test_add_num_limb(unsigned int number_of_tests, unsigned int seed) {
     return success;
 }
 
-//bool test_sub_num_num(unsigned int number_of_tests, unsigned int seed) {
-//    gmp_randstate_t gmp_random_state;
-//    gmp_randinit_default(gmp_random_state);
-//    gmp_randseed_ui(gmp_random_state, seed);
-//
-//    mpz_t op1_gmp;
-//    mpz_t op2_gmp;
-//    mpz_t res_gmp;
-//    mpz_t mod_gmp;
-//    mpz_init(op1_gmp);
-//    mpz_init(op2_gmp);
-//    mpz_init(res_gmp);
-//    mpz_init(mod_gmp);
-//
-//    limb_t op1[NUM_LIMBS];
-//    limb_t op2[NUM_LIMBS];
-//    limb_t res[NUM_LIMBS];
-//    limb_t borrow_in;
-//
-//    bool success = true;
-//
-//    for (unsigned int i = 0; (i < number_of_tests) && success; i++) {
-//        three_sorted_gmp operands = get_three_sorted_gmp(PRIME_FIELD_BINARY_BIT_LENGTH, gmp_random_state);
-//        /* op2 has to be smaller than op1 since this is normal subtraction (not modular) */
-//        mpz_set(op1_gmp, operands.middle);
-//        mpz_set(op2_gmp, operands.small);
-//        mpz_set(mod_gmp, operands.big);
-//
-//        clear_num(op1, NUM_LIMBS);
-//        clear_num(op2, NUM_LIMBS);
-//        clear_num(res, NUM_LIMBS);
-//        clear_num(&borrow_in, 1);
-//
-//        convert_gmp_to_num(op1, op1_gmp, NUM_LIMBS);
-//        convert_gmp_to_num(op2, op2_gmp, NUM_LIMBS);
-//
-//        mpz_sub(res_gmp, op1_gmp, op2_gmp);
-//        sub_num_num(res, op1, op2, NUM_LIMBS, borrow_in);
-//
-//        if (!is_equal_num_gmp(res, res_gmp, NUM_LIMBS)) {
-//            sub_num_num(res, op1, op2, NUM_LIMBS, borrow_in);
-//            print_num_gmp(op1_gmp, NUM_LIMBS);
-//            print_num(op1, NUM_LIMBS);
-//            print_num_gmp(op2_gmp, NUM_LIMBS);
-//            print_num(op2, NUM_LIMBS);
-//            print_num_gmp(res_gmp, NUM_LIMBS);
-//            print_num(res, NUM_LIMBS);
-//            success = false;
-//        }
-//
-//        clear_three_sorted_gmp(operands);
-//    }
-//
-//    mpz_clear(op1_gmp);
-//    mpz_clear(op2_gmp);
-//    mpz_clear(res_gmp);
-//    mpz_clear(mod_gmp);
-//    gmp_randclear(gmp_random_state);
-//
-//    return success;
-//}
-//
+bool test_sub_num_num(unsigned int number_of_tests, unsigned int seed) {
+    gmp_randstate_t gmp_random_state;
+    gmp_randinit_default(gmp_random_state);
+    gmp_randseed_ui(gmp_random_state, seed);
+
+    gmp_int_t op1_gmp;
+    gmp_int_t op2_gmp;
+    gmp_int_t res_gmp;
+    gmp_int_t mod_gmp;
+    gmp_int_init(op1_gmp);
+    gmp_int_init(op2_gmp);
+    gmp_int_init(res_gmp);
+    gmp_int_init(mod_gmp);
+
+    limb_t op1[NUM_LIMBS];
+    limb_t op2[NUM_LIMBS];
+    limb_t res[NUM_LIMBS];
+    limb_t borrow_in;
+
+    bool success = true;
+
+    for (unsigned int i = 0; (i < number_of_tests) && success; i++) {
+        three_sorted_gmp operands = get_three_sorted_gmp(PRIME_FIELD_BINARY_BIT_LENGTH, gmp_random_state);
+        /* op2 has to be smaller than op1 since this is normal subtraction (not modular) */
+        gmp_int_set(op1_gmp, operands.middle);
+        gmp_int_set(op2_gmp, operands.small);
+        gmp_int_set(mod_gmp, operands.big);
+
+        zero_num(op1, NUM_LIMBS);
+        zero_num(op2, NUM_LIMBS);
+        zero_num(res, NUM_LIMBS);
+        zero_num(&borrow_in, 1);
+
+        convert_gmp_to_num(op1, op1_gmp, NUM_LIMBS);
+        convert_gmp_to_num(op2, op2_gmp, NUM_LIMBS);
+
+        gmp_int_sub(res_gmp, op1_gmp, op2_gmp);
+        sub_num_num(res, op1, op2, NUM_LIMBS, borrow_in);
+
+        if (!is_equal_num_gmp(res, res_gmp, NUM_LIMBS)) {
+            sub_num_num(res, op1, op2, NUM_LIMBS, borrow_in);
+            print_num_gmp(op1_gmp, NUM_LIMBS);
+            print_num(op1, NUM_LIMBS);
+            print_num_gmp(op2_gmp, NUM_LIMBS);
+            print_num(op2, NUM_LIMBS);
+            print_num_gmp(res_gmp, NUM_LIMBS);
+            print_num(res, NUM_LIMBS);
+            success = false;
+        }
+
+        clear_three_sorted_gmp(operands);
+    }
+
+    gmp_int_clear(op1_gmp);
+    gmp_int_clear(op2_gmp);
+    gmp_int_clear(res_gmp);
+    gmp_int_clear(mod_gmp);
+    gmp_randclear(gmp_random_state);
+
+    return success;
+}
+
 //bool test_mul_limb_limb(unsigned int number_of_tests, unsigned int seed) {
 //    gmp_randstate_t gmp_random_state;
 //    gmp_randinit_default(gmp_random_state);
@@ -689,16 +689,16 @@ void check_add_num_limb() {
     printf("\n");
 }
 
-//void check_sub_num_num() {
-//    printf("Sub:\n");
-//    if (test_sub_num_num(NUM_ITERATIONS, SEED)) {
-//        printf("Success\n");
-//    } else {
-//        printf("Failed\n");
-//    }
-//    printf("\n");
-//}
-//
+void check_sub_num_num() {
+    printf("Sub:\n");
+    if (test_sub_num_num(NUM_ITERATIONS, SEED)) {
+        printf("Success\n");
+    } else {
+        printf("Failed\n");
+    }
+    printf("\n");
+}
+
 //void check_mul_limb_limb() {
 //    printf("mul_limb_limb:\n");
 //    if (test_mul_limb_limb(NUM_ITERATIONS, SEED)) {
@@ -775,18 +775,19 @@ int main(void) {
         check_add_num_num();
         check_add_num_limb();
         check_sub_num_num();
-        check_mul_limb_limb();
-        check_mul_num_limb();
-        check_mul_num_num();
-        check_cmp_num_num();
-        check_add_mod_num_num();
-        check_sub_mod_num_num();
-        check_mul_montgomery_num_num();
+//        check_mul_limb_limb();
+//        check_mul_num_limb();
+//        check_mul_num_num();
+//        check_cmp_num_num();
+//        check_add_mod_num_num();
+//        check_sub_mod_num_num();
+//        check_mul_montgomery_num_num();
 
     #elif SIMD_PARALLEL_WALKS
 
         check_add_num_num();
         check_add_num_limb();
+        check_sub_num_num();
 
 //        gmp_randstate_t gmp_random_state;
 //        gmp_randinit_default(gmp_random_state);
