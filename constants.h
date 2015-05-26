2f6d110c66fb5c767db1b617f1cf9350a0062712
Sahand Kashani-Akhavan <sahand.kashani@gmail.com>
2015-05-26 17:04:53 +0200

    compile-time division to obtain NUM_LIMBS

--- constants.h
+++ constants.h
@@ -26,7 +26,7 @@
     /* constants */
     #define BASE_EXPONENT               ((FULL_LIMB_PRECISION * LIMB_SIZE_IN_BITS) + ((!FULL_LIMB_PRECISION) * (LIMB_SIZE_IN_BITS - 1)))
     #define NUM_EXCESS_BASE_BITS        (LIMB_SIZE_IN_BITS - BASE_EXPONENT)
-    #define NUM_LIMBS                   ((unsigned int) ceil((PRIME_FIELD_BINARY_BIT_LENGTH)/ ((double) BASE_EXPONENT)))
+    #define NUM_LIMBS                   ((PRIME_FIELD_BINARY_BIT_LENGTH + BASE_EXPONENT - 1) / BASE_EXPONENT) /* equivalent to ((unsigned int) ceil((PRIME_FIELD_BINARY_BIT_LENGTH)/ ((double) BASE_EXPONENT))) */
     #define PRIME_FIELD_FULL_HEX_LENGTH (NUM_LIMBS * LIMB_SIZE_IN_HEX)
 
 #endif /* CONSTANTS_H_ */
