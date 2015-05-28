18ff44dc328922b813ee78b888c55b046cc76c38
Sahand Kashani-Akhavan <sahand.kashani@gmail.com>
2015-05-28 15:13:00 +0200

    added constants for modulus and (R^2 mod m) for 131-bit field

--- constants.h
+++ constants.h
@@ -1,11 +1,15 @@
 #ifndef CONSTANTS_H_
 #define CONSTANTS_H_
 
+    #include "limb.h"
     #include "settings.h"
 
     #define BASE_EXPONENT               ((FULL_LIMB_PRECISION * LIMB_SIZE_IN_BITS) + ((!FULL_LIMB_PRECISION) * (LIMB_SIZE_IN_BITS - 1)))
     #define NUM_EXCESS_BASE_BITS        (LIMB_SIZE_IN_BITS - BASE_EXPONENT)
     #define NUM_LIMBS                   ((PRIME_FIELD_BINARY_BIT_LENGTH + BASE_EXPONENT - 1) / BASE_EXPONENT) /* equivalent to ((unsigned int) ceil((PRIME_FIELD_BINARY_BIT_LENGTH)/ ((double) BASE_EXPONENT))) */
     #define PRIME_FIELD_FULL_HEX_LENGTH (NUM_LIMBS * LIMB_SIZE_IN_HEX)
+
+    extern limb_building_block_t m[NUM_ENTRIES_IN_LIMB * NUM_LIMBS];
+    extern limb_building_block_t R_2_mod_m[NUM_ENTRIES_IN_LIMB * NUM_LIMBS];
 
 #endif /* CONSTANTS_H_ */
