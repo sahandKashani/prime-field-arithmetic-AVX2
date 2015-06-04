06139c203ea20209840fbe7432984cbdd20a4c68
Sahand Kashani-Akhavan <sahand.kashani@gmail.com>
2015-06-04 12:26:50 +0200

    commit for benchmark script writing on local machine

--- main.c
+++ main.c
@@ -1,12 +1,22 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/time.h>
+#include <sys/resource.h>
 #include "compile_time_checks.h"
 #include "constants.h"
 #include "check_arithmetic.h"
 #include "settings.h"
+#include "elliptic_curve.h"
+#include "utilities.h"
+
+double get_time() {
+    struct timeval t;
+      gettimeofday(&t, NULL);
+    return (double) t.tv_sec + (double) t.tv_usec*1e-6;
+}
 
 int main(void) {
-    unsigned int number_of_points = 1 << 10;
+    unsigned int number_of_points = 1 << 15;
     initialize_constants(number_of_points);
 
     #if TEST
@@ -25,6 +35,21 @@ int main(void) {
         check_neg_point(number_of_points);
 
     #elif BENCHMARK /* TEST */
+
+        struct curve_point p1;
+        struct curve_point p2;
+        copy_num(p1.x, points_x_glo[0], NUM_LIMBS);
+        copy_num(p1.y, points_y_glo[0], NUM_LIMBS);
+        copy_num(p2.x, points_x_glo[1], NUM_LIMBS);
+        copy_num(p2.y, points_y_glo[1], NUM_LIMBS);
+
+        double start = get_time();
+        for (unsigned int i = 0; i < (number_of_points / NUM_ENTRIES_IN_LIMB); i++) {
+            add_point_point(&p2, &p2, &p1, NUM_LIMBS);
+        }
+        double stop = get_time();
+
+        printf("elapsed time = %lf s\n", stop - start);
 
     #endif /* BENCHMARK */
 
--- settings.h
+++ settings.h
@@ -2,14 +2,14 @@
 #define SETTINGS_H_
 
     #define PRIME_FIELD_BINARY_BIT_LENGTH        (131)
-    #define LIMB_SIZE_IN_BITS                    (32) /* 8, 16, 32, 64 */
-    #define FULL_LIMB_PRECISION                  (1)
+    #define LIMB_SIZE_IN_BITS                    (64) /* 8, 16, 32, 64 */
+    #define FULL_LIMB_PRECISION                  (1) /* 0, 1 */
 
-    #define MULX                                 (0)
+    #define MULX                                 (0) /* 0, 1: Only useful if LIMB_SIZE_IN_BITS == 64 */
 
-    #define SIMD_PARALLEL_WALKS                  (0)
+    #define SIMD_PARALLEL_WALKS                  (0) /* 0, 1 */
 
-    #define TEST                                 (1)
-    #define BENCHMARK                            (0)
+    #define TEST                                 (0) /* 0, 1 */
+    #define BENCHMARK                            (1) /* 0, 1 */
 
 #endif /* SETTINGS_H_ */
