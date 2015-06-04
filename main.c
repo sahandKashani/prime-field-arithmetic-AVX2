ca27b449e8d03fe9e01ac00d9e117be1a66f344a
Sahand Kashani-Akhavan <sahand.kashani@gmail.com>
2015-06-04 14:28:01 +0200

    prime field arithmetic benchmarks

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 #include "constants.h"
 #include "check_arithmetic.h"
 #include "settings.h"
+#include "prime_field.h"
 #include "elliptic_curve.h"
 #include "utilities.h"
 
@@ -72,6 +73,9 @@ int main(void) {
             stop = get_time();
             printf("double_point_gmp = %lf s\n", stop - start);
 
+            curve_point_clear_gmp(&p1_gmp);
+            curve_point_clear_gmp(&p2_gmp);
+
         #else
 
             struct curve_point p1;
@@ -108,6 +112,85 @@ int main(void) {
 
         double start = 0;
         double stop = 0;
+
+        #if BENCHMARK_USE_GMP
+
+            gmp_int_t x_gmp;
+            gmp_int_t y_gmp;
+            gmp_int_init(x_gmp);
+            gmp_int_init(y_gmp);
+
+            gmp_int_set(x_gmp, points_x_glo_gmp[0]);
+            gmp_int_set(y_gmp, points_y_glo_gmp[0]);
+
+            start = get_time();
+            for (unsigned int i = 0; i < (number_of_points / NUM_ENTRIES_IN_LIMB); i++) {
+                gmp_int_add_mod(x_gmp, x_gmp, y_gmp, m_glo_gmp);
+            }
+            stop = get_time();
+            printf("gmp_int_add_mod = %lf s\n", stop - start);
+
+            start = get_time();
+            for (unsigned int i = 0; i < (number_of_points / NUM_ENTRIES_IN_LIMB); i++) {
+                gmp_int_sub_mod(x_gmp, x_gmp, y_gmp, m_glo_gmp);
+            }
+            stop = get_time();
+            printf("gmp_int_sub_mod = %lf s\n", stop - start);
+
+            start = get_time();
+            for (unsigned int i = 0; i < (number_of_points / NUM_ENTRIES_IN_LIMB); i++) {
+                gmp_int_mul_montgomery(x_gmp, x_gmp, y_gmp, inv_R_glo_gmp, m_glo_gmp);
+            }
+            stop = get_time();
+            printf("gmp_int_mul_montgomery = %lf s\n", stop - start);
+
+            start = get_time();
+            for (unsigned int i = 0; i < (number_of_points / NUM_ENTRIES_IN_LIMB); i++) {
+                gmp_int_montgomery_inverse(x_gmp, y_gmp, m_glo_gmp);
+            }
+            stop = get_time();
+            printf("gmp_int_montgomery_inverse = %lf s\n", stop - start);
+
+            gmp_int_clear(x_gmp);
+            gmp_int_clear(y_gmp);
+
+        #else
+
+            limb_t x[NUM_LIMBS];
+            limb_t y[NUM_LIMBS];
+
+            copy_num(x, points_x_glo[0], NUM_LIMBS);
+            copy_num(y, points_y_glo[0], NUM_LIMBS);
+
+            start = get_time();
+            for (unsigned int i = 0; i < (number_of_points / NUM_ENTRIES_IN_LIMB); i++) {
+                add_mod_num_num(x, x, y, m_glo, NUM_LIMBS);
+            }
+            stop = get_time();
+            printf("add_mod_num_num = %lf s\n", stop - start);
+
+            start = get_time();
+            for (unsigned int i = 0; i < (number_of_points / NUM_ENTRIES_IN_LIMB); i++) {
+                sub_mod_num_num(x, x, y, m_glo, NUM_LIMBS);
+            }
+            stop = get_time();
+            printf("sub_mod_num_num = %lf s\n", stop - start);
+
+            start = get_time();
+            for (unsigned int i = 0; i < (number_of_points / NUM_ENTRIES_IN_LIMB); i++) {
+                mul_montgomery_num_num(x, x, y, m_glo, m_prime_glo, NUM_LIMBS);
+            }
+            stop = get_time();
+            printf("mul_montgomery_num_num = %lf s\n", stop - start);
+
+            start = get_time();
+            for (unsigned int i = 0; i < (number_of_points / NUM_ENTRIES_IN_LIMB); i++) {
+                montgomery_inverse_num(x, y, m_glo, NUM_LIMBS);
+            }
+            stop = get_time();
+            printf("montgomery_inverse_num = %lf s\n", stop - start);
+
+        #endif
 
     #endif /* BENCHMARK */
 
